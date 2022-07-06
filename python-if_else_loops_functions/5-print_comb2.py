bb3392b02c09e7fe804b8bdf106adcc15b122a67
Moussa-13 <m.amzat@alustudent.com>
2022-07-06 07:28:59 -0700

    00...99

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Print numbers from 0 to 99
+# Numbers must be separated by ',' followed by a space
+# Numbers should be printed in ascending order, with two digits
+
+
+for number in range(0, 100):
+    print("{:02d}".format(number), end=", ")
+print(number)
