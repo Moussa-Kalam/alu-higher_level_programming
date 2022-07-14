408e205f901b917698afc7d13782c8ed0c977614
Moussa-13 <m.amzat@alustudent.com>
2022-07-14 02:36:54 -0700

    Replace element

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,6 +3,8 @@
 # at a specific position (like in C)
 
 def replace_in_list(my_list, idx, element): 
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > len(my_list) -1:
         return my_list
-    return my_list[element]
+    else:
+        my_list[idx] = element
+        return my_list
