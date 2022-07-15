59d9535c568928d6d6b592a105eafb42cecc3237
Moussa-13 <m.amzat@alustudent.com>
2022-07-15 12:19:11 -0700

    Number of keys

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # A function that returns the number of keys in a dictionary
-def number_keys(a_ dictionary):
+
+
+def number_keys(a_dictionary):
     num = 0
     list_keys = list(a_dictionary.keys())
 
