bf6071e4731aa12e4eeb1a3da98d9cde638a9a26
Moussa-13 <m.amzat@alustudent.com>
2022-07-15 09:12:53 -0700

    More returns!

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# A function that returns a tuple with the length of a string and its first character
-
+# A function that returns a tuple with the length of a string
+# and its first character
 
 
 def multiple_returns(sentence):
