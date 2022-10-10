74cb440a1dcf8e345e0065bf071f03ffa6c66336
Moussa-Kalam <m.amzat@alustudent.com>
2022-10-10 18:16:20 +0200

    I love C

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const args = process.argv.slice(2);
 if (isNaN(parseInt(args[0]))) {
   console.log('Missing number of occurrences');
 } else {
-  for (i = 0; i < args[0]; i++) {
+  for (let i = 0; i < args[0]; i++) {
     console.log('C is fun');
   }
 }
