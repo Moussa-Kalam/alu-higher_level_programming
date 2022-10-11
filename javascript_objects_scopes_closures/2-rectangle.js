b222b19a5102fc9c2c5fbdf87f09dbb1ae89b461
Moussa-Kalam <m.amzat@alustudent.com>
2022-10-11 16:49:58 +0200

    Rectangle #2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,11 +2,11 @@
 // A class Rectangle that defines a rectangle
 class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.height = h;
     if (w == null || h == null) return null;
     if (w <= 0 || h <= 0) return null;
-    }
+    this.width = w;
+    this.height = h;
   }
+}
 
 module.exports = Rectangle;
