0c2fa8d776bfcd419b90a8c50959d19b272c31e5
Moussa-Kalam <m.amzat@alustudent.com>
2022-10-11 17:20:11 +0200

    Rectangle #4

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+// Class Rectangle that defines a rectangle
+
+class Rectangle {
+  constructor (w, h) {
+    if (w == null || h == null) return null;
+    if (w <= 0 || h <= 0) return null;
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    const width = 'X'.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(width);
+    }
+  }
+
+  rotate () {
+    const val = this.width;
+    this.width = this.height;
+    this.height = val;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
