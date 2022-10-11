7d8ab9ce081881710fe32a964cfb77d0c96be707
Moussa-Kalam <m.amzat@alustudent.com>
2022-10-11 16:47:57 +0200

    Rectangle #2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -4,11 +4,9 @@ class Rectangle {
   constructor (w, h) {
     this.width = w;
     this.height = h;
-    if (w == null || h == null) {
-      return null;
-    } else if (w <= 0 || h <= 0) {
-      return null;
+    if (w == null || h == null) return null;
+    if (w <= 0 || h <= 0) return null;
     }
   }
-}
+
 module.exports = Rectangle;
