5cc441239c97bfa2d71344f071fea74ea61829a3
Moussa-Kalam <m.amzat@alustudent.com>
2022-10-12 15:42:32 +0200

    Sorted occurences

--- javascript_objects_scopes_closures/101-sorted.js
+++ javascript_objects_scopes_closures/101-sorted.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
-// Import a dictionary of occurrences by user id 
+// Import a dictionary of occurrences by user id
 // Compute a dictionary of user ids by occurrence
 
 const dict = require('./101-data.js').dict;
 const newDict = {};
 for (const key in dict) {
-    const value = dict[key];
-    if (newDict[value] === undefined) {
-        newDict[value] = [];
-        newDict[value].push(key);
-    } else {
-        newDict[value].push(key);
-    }
+  const value = dict[key];
+  if (newDict[value] === undefined) {
+    newDict[value] = [];
+    newDict[value].push(key);
+  } else {
+    newDict[value].push(key);
+  }
 }
 console.log(newDict);
