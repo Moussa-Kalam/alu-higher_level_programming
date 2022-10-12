296508473bd0e8d356f647823f19c9362a81c780
Moussa-Kalam <m.amzat@alustudent.com>
2022-10-12 10:26:04 +0200

    Esrever

---
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Function that returns the reversed version of a list
+
+exports.esrever = function (list) {
+  const output = [];
+  let i = list.length - 1;
+  for (i; i >= 0; i--) {
+    output.push(list[i]);
+  }
+  return output;
+};
