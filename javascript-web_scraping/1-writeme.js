ab166f1fbf870751b0363db38838d2c235a18dc3
Moussa-Kalam <m.amzat@alustudent.com>
2022-10-14 11:24:47 +0200

    Write me

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// A script that writes a string to a file
+const args = process.argv.slice(2);
+const fs = require('fs');
+
+fs.writeFile(args[0], args[1], 'utf-8', (err) => {
+  if (error) {
+    console.log(error);
+  }
+});
