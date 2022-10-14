940fa7739bf366e19f116ebaeeb9c1581b0d8167
Moussa-Kalam <m.amzat@alustudent.com>
2022-10-14 12:40:38 +0200

    Status code

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,10 +3,10 @@
 const url = process.argv.slice(2)[0];
 const request = require('request');
 
-request(url, (err, response, body) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(`code: ${reponse.statusCode}`);
-    }
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
 });
