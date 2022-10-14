cd30518212e596993a8955355dd8b372111c7a86
Moussa-Kalam <m.amzat@alustudent.com>
2022-10-14 12:25:04 +0200

    Write me

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const args = process.argv.slice(2);
 const fs = require('fs');
 
 fs.writeFile(args[0], args[1], 'utf-8', (err) => {
-  if (error) {
-    console.log(error);
+  if (err) {
+    console.log(err);
   }
 });
